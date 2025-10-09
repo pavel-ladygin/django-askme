140c8429385c43301edcddd30d7944314e0170e9
Pavel <Ladygin_2005q@mail.ru>
2025-10-09 20:52:41 +0300

    create base html pages

--- askme_ladygin/main/urls.py
+++ askme_ladygin/main/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home")
+    path("", views.home),
+    path("question/", views.question_detail),
+    path("ask/",views.ask),
+    path("login/", views.login),
+    path("register/", views.register),
 ]
--- askme_ladygin/main/views.py
+++ askme_ladygin/main/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 def home(request):
     return render(request,"pages/index.html")
+
+def question_detail(request):
+    return render(request, "pages/question.html")
+
+def ask(request):
+    return render(request, "pages/ask.html")
+
+def login(request):
+    return render(request, "pages/login.html")
+
+def register(request):
+    return render(request, "pages/register.html")
